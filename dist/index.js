56cee35e57b43bc010a29d0426a1e961fd9bae1d
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2022-02-21 20:12:19 +0100

    210222 installations

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+
+console.log("Hi, this is the me");
+
+/******/ })()
+;
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+console.log ("Hi, this is the me"); 
